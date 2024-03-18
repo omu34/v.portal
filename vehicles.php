f4deea6f9c3586106d58f356d77464b932520a00
Bernard Omuse <bernardomuse22@gmail.com>
2024-03-18 21:27:06 +0300

    Vehicle Portal

---
+++ vehicles.php
@@ -0,0 +1,285 @@
+<?php
+
+
+"   
+    'Model Volkswagen, TOURAN TSI'
+    ['Front', 'H9tuKacbvA2gkWCHBtBOkBhCeptZHQHeO5oSgmXY.png']
+    ['Back', 'IlNL4JDIwJkEXiKNcWB83wv0q5Y1GVeK0gTFoOkI.png']
+
+    'Model Toyota, Probox'
+    ['Front', 'IufxgjTTronN1gaxX7w6j0EwYXGBQ0LKyId0ZbMZ.jpeg']
+    ['Front', 'Bo83ftTyh8KqXsPZb6XeZGh7d6uSXdUmDFOD5OwK.jpeg']
+
+    'Model Isuzu, FTR 90'
+    ['Back Left', 'EvyInDOPO5MnQCcI8esYV0LfVJKZHKr3uCy1MndM.jpeg']
+    ['Back Left', 'EhaiuBEfxRXZu8vBtwzJ4UIrGZVolruIiknSPFRD.jpeg']
+
+    'Model Tata, 25181'
+    ['Front ', '8JIgapusN1q4ZmOdyBxZFA0t0O7DVBfyJnlUQyD3.jpeg']
+    ['sideways Front Left', '2niOVuTBMpwctU6eLohyw0PPYD3cVXC7Qf4OGVlg.jpeg']
+
+    'Model  Isuzu, FRR'
+    ['Front Left', 'HLZ8jX76iKWqsr806ev3HNF7KmkMUrq3xaK0FEhs.jpeg']
+    ['Back Right', 'bi2soqKUq32752SUmgCdZYErTyNWMatEOnL3IOSY.jpeg']
+    ['Front', 'HLZ8jX76iKWqsr806ev3HNF7KmkMUrq3xaK0FEhs.jpeg']
+
+
+
+    'Model Toyota, Hilux'
+    ['sideways', '8XNfzXIZudaOtwmHqTLUcqeaRekzThojQS9NliId.jpeg']
+    ['Back Right', 'P3onfMlxico1HW9qQqVfmNbALHSZuUQvjE0pkGZ9.jpeg']
+
+    'Model Isuzu, NQR'
+    ['Back Left', 'GPNZtKcIJA5pgJcDjlcRJkJWY2Wf9SEsBeSh0i9Z.jpeg']
+    ['Front Left', '2zet3JYLm3QmCuO4HK8y27gSvNZAdpuAkZbCh43I.jpeg']
+
+    'Model Isuzu, FTR'
+    ['Inside Drivers', 'fmKCa0R9UgnijnVwC7gmnujETqa5Oo463Qf2f5ty.jpeg']
+    ['sideways Right', 'Z78ZEJ9O6pLKotrScE9eISD0ndI1hNbwcsQkLc4j.jpeg']
+    ['Trailer', 'rMcbzXYAlbEq8li7j47yMm2TD2pokMb4tyelE1eQ.jpeg']
+    ['Front', 'BZCv0PPVrRSS1HIH1tDAWDL8hxwjtmEjxiPBwNlo.jpeg']
+
+    'Model Toyota, Ractis'
+    ['Back Left', 'Ldu5WlRRzsYoqs4wjvs0pxVETXK30f8LoIDeQrci.jpeg']
+    ['Back Right', 'pwbmm5JndaOOfL5zoOkqYyEXok7A8x22kRcRtlhZ.jpeg']
+
+    'Model Isuzu, FRR90'
+    ['Front Right', 'M0W5smphc0sJtdhuZmErnKD1ZcdRI9AZWaGwjMIR.png']
+    ['Back Right', 'Gh3BceHX69CPHzwUEZ2hUvoig5FVvbxiToKziy5X.png']
+
+    'Model Isuzu, FTR'
+    ['Sideways ', 'g0HuMulQ0mogon1lCqGR3y26LuFDbn67FrfgmI3L.jpeg']
+    ['Back', 'WX8y9zSfme9x18nKk3omUNdNpxj9Tak4uA6zWSlW.jpeg']
+
+    'Model Toyota, Hilux'
+    ['Front Right', 'u1RiNF01dYwxBdqVpZ7TaJ5fx7OuELADrl48nLKm.jpeg']
+    ['Engine', 'EViKDO0euaKjTNQHbonurscYWiyzhgZfpa9kRwpZ.jpeg']
+    ['Inside Driver seat', 'mTX5tRn3wDZbTY6jniY1bkTA0F6A6ptpT47DElAv.jpeg']
+    ['Back Left', 'Ke1dl7PXbh9BU8tfK5scM47Zn8m3Nt473G6SJmqy.jpeg']
+    ['Front Right', 'sZe7NFlHF6bLVfTZfkP60ft8jjMvjbuviv5qunIo.jpeg']
+    ['Front Tire', 'cU0lltTIGtcpk2U7MZR9vAMEZM937NobQ40wXFrJ.jpeg']
+    ['Back Right ', '8xA4B8PX70wIFwfKqbG7w40e6DJDObleBUSlaclb.jpe']
+    
+    'Model Mitsubishi, Fuso FIVI'
+    ['Back Right', 'tGdhWORb739o1NMov38yKbT6wfHloTvRBNdPFdyg.jpeg']
+    ['Sideways Front', 'NagxfMfzRxMgdhdguVIdpjsHw4lfs34pIzuy54X0.jpeg']
+    ['Sideways Back Left', 'IaV9lZeNjeeawzZc2FUOmmPaztgZMVejTjN0zm3C.jpe']
+
+    'Model Tata, Tipper'
+    ['Front', 'Ab3jJcMbfeq6Q2ZiYwKT77MHldaEHIBq3pGmwrXj.jpeg']
+    ['Sideways Back Right', 'yhAWPEiE9OGrhmKeUpJB8FrYltrlXa844MNCfknJ.jpeg']
+    ['Sideways Front Right', 'iw8TUpOvDMgrpp0zHh5MuEzkgfIXnUAuFKSZ7XqQ.jpeg']
+
+    'Model Agro, Trailer'
+    ['Front Right', '5incQfoXkGwQlDW9AY9WeKBT1qK0lBXaISl15WPa.jpeg']
+    ['Front', 'NSWyJJFucB5MJefpKPfgEFRd2ka0n5LwGscsgx4R.jpeg']
+    ['Sideways Back Right', '154zHsgkFoJG8pdyTiLEzWs1oSu9Kcp5I2E8NfBq.jpeg']
+    ['Back', 'b8UaaRPOerSC4AO8nJcRuoAsObHvkvylT6IdUBiH.jpeg']
+
+    'Model Isuzu, GXZ60'
+    ['Front Right', 'yLgJguJgAcIntwKiHBA2ixFQ6cuFguUZf4gxkZhp.jpeg']
+    ['Back Tires', 'e0PXw1ErNie0LsmeGSOQne9L7W8AlBOh48tGBRUg.jpeg']
+    
+
+
+    'Model Nissan, XTrail'
+    ['Front', 'gxFgGJ7YWgwLvH4ftQKX9Hdazxr0ro2CKyTpxLnW.png']
+    ['Back', 'N0eOfhWm06ruCNMmefRYH1zUqapSzLV8bciUF10U.jpeg']
+    ['Sideways', 'ZYAkFHBR7YohIfZdG7LvjwzEyIhhJratWu1xfAVW.png']
+
+    'Model Isuzu, NKR'
+    ['Front', 'e59JjgqxDgkeGzoW6Aan83qV6wj50sYQJJkc4b2Z.jpeg']
+    ['Sideways Front', 'h6CUz7tXeC1acCE1WoSev2ibjzmqWWJdwFBfFRFp.jpeg']
+    ['Inside Seats ', 'yprIJkl9HDZitzYk2lUfVdZB6C5hRnqGkuOV7GlF.jpeg']
+    ['Front', 'H9tuKacbvA2gkWCHBtBOkBhCeptZHQHeO5oSgmXY.png']
+    ['Sideways Back Left', '9Sq72Zv74ihMv8TDNksMXXAkqQn1rw7xsMX9e3Pd.jpeg']
+
+    'Model Isuzu, FTS'
+    ['Back', 'Ckss9oFxZCyfGU1cMDGRIYAq7CYoBncHV2SLvBAh.jpeg']
+    ['Front Right', 'sbMr2L9QfPkPSkfAqq5h3ghrGAp0atQfA06l0dCU.jpeg']
+    ['Front Left', 'c3vRhJ5Fc7pjgP6xvxtiYB2I7CHgIvacFqKtOhi7.jpeg']
+    ['Inside Lorry', 'nwm4l7HGajTaIhHZye9c2yTzUiDhzAFpN1ezT3XY.jpeg']
+    ['Front Left', 'DWgvtXjqEknkOWO1wzIN4XyPJYHv5jTTgBsnPHMH.jpeg']
+
+    'Model Isuzu, FTS'
+    ['Back', 'Z9Svfs9ER4jGBLPfiddWQQmZjdgbJ3PCTeRvJAWG.jpeg']   
+    ['Inside Driver', 'z30rf0r3nwE9nc9oSpHY51erDbFlyUVWg3P3obUM.jpeg']
+    ['Front Left','JiY9LYxD0iHH2WCOpM3FVdhWiBu83zfTiVMjBA0a.jpeg']
+
+    'Model Isuzu, FTS'
+    ['Back', '3qZVlRT39wg01wpvgs7Rl2JwVT87mTj5NSn5IC0L.jpeg']
+    ['Sideways Back Left', 'yNx11KskjVnDv6zXOMy79VSKA4qB0axSDcbZT3v4.jpeg']    
+    ['Sideways Front Right', 'EfB4AWUqvFpILlkCgVcWvIIhuF2aljoRZHcpfM8A.jpeg']
+    ['Front', 'nCOXaERYvQ7PMW0PAtBOXFN2wXIAfhbU0YA1iuiJ.jpeg']
+    ['Front Right','NIlXjP7JkII1IJkqCVQCnvoiwHgRg5wk3J1vMXio.jpeg']
+    ['Back Right','c5HxClV2w4YOrLUunDkuSzMIcEPDR7D6X5bH5uBS.jpeg']
+    ['Sideways Left','CHyD1HRMmEHgr86C1m2Z4duBdOb6kthVNlf0W5BU.jpeg']
+    ['Sideways  Right','3pIMsfMX7411g99BpKtFnkfRS38ZVuTrP8SF0YBk.jpeg']
+
+    'Model Isuzu, FTS'
+    ['Sideways Right', 'ggkYzMOjyJtVIMZAHv51yzbutxt0OplcB7bpeR95.jpeg']
+    ['Front', 'K4H7Y7j6XJxnWgGeQmK0PRoneqCUAEt2tj2V0iuE.jpeg']    
+    ['Back', 'BXmPyVrT0zryGkRyOqcqCyoNbR9PUS8zSjPee39c.jpeg']
+    ['Front Right', 'srevvVHK8uffbWgzdxCSyXLban2IMyZx8kSsXBS0.jpeg']
+    ['Inside Driver','sVm8tVWfuulUkrOrmbjLflVskWUFvPOWD968mgDJ.jpeg']
+    ['Sideways Right','qoipxOPc4maga5qDtg85NjA5KXlfnf4iUHB0od9l.jpeg']
+    ['Sideways Left','dMN0Uns7wgEYPSRjS1jV2HopgE35uYP0zlEI309F.jpeg']
+   
+    'Model Isuzu, FTS'
+    ['Sideways Left', 'SlXsuMAOPL4hQGVlElf9pUFYCs5jBZYJmUw2tZH7.jpeg']
+    ['Front Left', '2LxXi8ieJwYRQj7pB7ubmXRcgrhS4tMhXXuiQeMC.jpeg']    
+    ['Back', 'Mr9nDGs8QlhQ0iKbWfc4o062AxFdm3KN5HVHDL5z.jpeg']
+    ['Sideways Front', 'Gk0y9EJ20IL0gHkAGf4gnijFQKL7c82tqYk8A1it.jpeg']
+    ['Inside Driver','nXc0Bx8tRzJ8ZKRY1YivnthGB9KufIt9LJ6beiPg.jpeg']
+    ['Sideways Right','MJIdOo5ev5bIxDnodCjjxd0rifLD6wWLXPJYf5lL.jpeg']
+    ['Front','2LxXi8ieJwYRQj7pB7ubmXRcgrhS4tMhXXuiQeMC.jpeg']
+
+    
+    
+    
+    'Model Isuzu, FTS'
+    ['Front', 'Jb856hgNuAldOYHfjiTmD5KXNRatFVVpu8bM6OGc.jpeg']    
+    ['Back', 'mpI2vqbcu2TIsTo4gWgsSVxpIMfuWLCfKu7QYAlz.jpeg']
+    
+    'Model Isuzu, FTS'
+    ['Back', 'mqCcIYJVGLBtWD5CfphCWYYAtXCxkstWHzjmdTz7.jpeg']
+    ['Back Right', 'W0epePiLzu9dx4Y8BvcTFE21VxWx22EUWWxs5wIg.jpeg']
+    ['Front', 'jZjTJGbHvkINEB6Pzpauh31OhjBHST3s14tyneXd.jpeg']
+    ['Front Right','9ugZqKJvpGD7L0W94ifVQasrqFOTNtpY1P8v050S.jpeg']
+    
+    'Model Isuzu, FTS'
+    ['Back', 'mqCcIYJVGLBtWD5CfphCWYYAtXCxkstWHzjmdTz7.jpeg']
+    ['Sideways Back', 'lklEtH03lL3fB822DPXlDn0i5g7QVkveVMfqqYNo.jpeg']
+    ['Front', 'jZjTJGbHvkINEB6Pzpauh31OhjBHST3s14tyneXd.jpeg']
+    ['Front Right','9ugZqKJvpGD7L0W94ifVQasrqFOTNtpY1P8v050S.jpeg']
+
+    'Model Isuzu, FTS'
+    ['Back Tyres', 'CWCjYNRpAcDY73JO2mlZxs3xS1l8sK51nXkaKPKY.jpeg']
+    ['Sideways Back', 'W0epePiLzu9dx4Y8BvcTFE21VxWx22EUWWxs5wIg.jpeg']
+    ['Back', 'qpxgwbMo10EXt6qYdOa0y1Avd7mnOE7rTmxooamm.jpeg']
+    ['Front Right ','olAp4Wz65oHpYRHgVGhcq4iyLBSt84CrCeHCRBhV.jpe']
+    ['Tank', 'VhbWy1UyxJROPH6nFA3PR7VMl2KJ4DoPlrq1wfW0.jpeg']
+    
+    'Model Isuzu, FTS'
+    ['Sideways Front', 'W7BnHTSkCtp3l2PJvxNJbzqIYRCJWOphhEt7R1XL.jpeg']
+    ['Sideways', 'RmvARZddZwpAiaHr7GO2cfJKrgiSY61PmZ7gfct3.jpeg']
+    ['Sideways','xkd3goGPS8nEInr5WJ6gCW50hbZ8sWdLaduAJ3c0.jpeg']
+    ['Inside Driver','jRSIOPPzXafKEheabz9fzBeDPQzxlLRvIwox2vgE.jpeg']
+    
+    'Model Isuzu, FTS'
+    ['Sideways Front', '2ywj6AKtkRmNmFhjJN2tMW56ZksFJw6lLRxyWsSN.jpeg']
+    ['Back','xiUGbvxNBq67oLH28cA1jeuxyPx5iLYR88gubFnE.jpeg']    
+    ['Sideways Right', 'yGdZdvjTzdxovoUsyYZ0CtYRaSCsa4xJAnAeBXTH.jpeg']
+    ['Front Right','CCEsODslHB0npNWHDMjVNEgB3j3fSMBGz7UQIjmY.jpeg']
+    ['Inside Driver','YK7snrBMRsrOD9Ja5PvR6lvEcDVEwM1AOPAFB93M.jpeg']
+    
+    'Model Isuzu, FTS'
+    ['Front Right', 'eq54TJmwLMjW35uUUI2bzndN8NamCeMACdAeWcgF.jpeg']
+    ['Front','OUGU1rEp7t7QOCZnwLJ3ks3HYonhWVTkFMlzLdYf.jpeg']    
+    
+
+    
+    
+    
+    'Model Isuzu, FTS'
+    ['Front Right', 'w7k26VNjfWPdjdZUdahYtCCn2nJIajkqg3ieGawM.jpeg']
+    ['Back Left','r7uRrPtK7OoV5Zobtj5aHOwMf3DKqe11qFviq9Ks.jpeg']    
+    ['Front', 'uKOf9zRAbUhE8DB3AxqhkPs9oCMcdNVV1LPc1neA.jpeg']
+    ['Front Left','usredC6pKccz19hm3QuONl7Vbr2k8KnCxd56xV3q.jpeg']
+    
+    'Model Isuzu, FTS'
+    ['Sideways left', 'fmELIxVCDfwdPWhFsXu6KznexI0HORz2HOoT1lLQ.jpeg']
+    ['Sideways Back','HsQpbIZA2hQbYJ3jzuJROK9JJYuxuJtjYw45fbsx.jpeg']
+    ['Front','w8SKaeUDjh7UyCVyKcthkcJADL078SJCdSjKRUIc.jpeg']    
+    ['Sideways Front', 'MRlriKyZIJLWnOeoy9HhH93SqEFbAhyD50BiWRFC.jpeg']
+       
+    'Model Isuzu, FTS'
+    ['Sideways Front','w8SKaeUDjh7UyCVyKcthkcJADL078SJCdSjKRUIc.jpeg']    
+    ['Back', 'AISh64xzi63tqkbvzrba7ocAe91aJpHj1VjEigWL.jpeg']     
+    ['Sideways','fSBJiVph10wt593CVi6O0JxZZwcDqOC93QnIe7v6.jpeg']
+    ['Front Right','o4m8bvR8SSowG3IZDHdzJMmk42iz0bqGeZGKqplX.jpeg']    
+    ['Inside Driver', '1tnHptBAXGgL050aLz7xYb6aXxKNPGW5BRgpenoh.jpeg']
+      
+    'Model Isuzu, FTS'
+    ['Sideways Back','hLL3jewRufcQfob1fLu3gzhkqqeVrOgPCldnzWAB.jpeg']    
+    ['Front', 'GLdiZOBrrRdgA0zqn6AzHypEWw4EZdzmSN5ClqBR.jpeg']     
+    ['Back','nV5NNxjTbjntFUI1pQ1WRxb39ZlfB7ORAy5AW29q.jpeg']
+    ['Sideways Front','ow6cl8AZNcOOFXeGnB0h6CKxGKasbzaj4Dtxrfte.jpeg']    
+    ['Sideways Front', '1EuI0MtGHkpbHawHp2DxKDOr8rZvChLgL7f17u72.jpeg']
+    
+    'Model Isuzu, FTS'
+    ['Sideways Front Right','v5vlCpvwusIZdRJLqCMsObetGX9ALVK7ixxZcd5H.jpeg']    
+    ['Front', 'o4k6pHD7H6qqxBC9MuwJlt7u59X4gKbDJqDuoQCt.jpeg']     
+    ['Sideways Front Left','z6sJlUGvof2SECcLsb9ekA0zw4B9UboCMpl0Ki43.jpeg']
+    ['Sideways Front Right','S2eSJVHwmdfMKhbijmpe59akwICBJ2gyWroNKC7f.jpeg']    
+    ['Back', 'z0LyRaBGiqGhvXGQhRvjPK7rH1Sva5Z3eL7V05mc.jpeg']
+    ['Inside Driver', 'pOaBCNeBDJRzHoJZRmujC9hODKwqVuY7k0rf7go6.jpeg']
+     
+    'Model Isuzu, FTS'
+    ['Front','fidjqVhLjyrZYxB71xxrHybbE746RLpTL1m0i9Xe.jpeg']    
+    ['Inside Drivers ', 'zWkiwoal29qZziU6PjYC52GKMlQZnrftn35Yi55k.jpeg']     
+    ['Back','rQbLBDOLJxnJlFEcP1VtWHLWKzpwJ16rJ2VYiCdT.jpeg']
+    ['Sideways Front Right','gJFjRR58LVXwhzQeLx9nIS44JkC8yLXcbbKXM4cR.jpeg']    
+    ['Sideways Front Left', 'DMMYyWzwguKGH12vbkeXIAT3ijNysJmUKXKOUUOD.jpeg']
+    ['Sideways Back Left', '84YWXfN3jMAHSNUE7nCBfRbYKT6u0oUZGAxVNXRX.jpeg']
+ 
+    'Model Isuzu, FTS'
+    ['Sideways Back Left','ATAFOALSEqdRntJviAZw84A765g4tyIUWJGyTGGu.jpeg']    
+    ['Sideways Back Right', 'zUnbExBfvWpPYYWhI3MrC1Qz0JVpkPlcmUUzS3uV.jpeg']     
+    ['Sideways Front Left','LswANKucxxT2ttKmUlenCq0QczF8AmKRfNjMSqbK.jpeg']
+    ['Sideways Front Right','uploads/8T3oECkVZzO7Lp63NhwCqqVOe2bXsfpl0xXEeTtJ.jpeg']    
+    
+    'Model Isuzu, FTS'
+    ['Sideways Front Right','qAn2Ac24MKg5xbkJORkbRSXcnl5yrthzA2VwBbHU.jpeg']    
+    ['Sideways Front Left', '63UPbIDDWjJ1sM3tQSynTsB26O4bKzuA3MMsn1xF.jpeg']
+    
+    
+    
+
+
+
+
+
+    'Model Mitsubishi, Fuso'
+    ['Engine','p9d7OtYU6jIKRyyslvuY24HWFRSRIUOwBE8pT1C7.jpeg']    
+    ['Front ', 'vsrnCZFxXFEYC6JrPs0XMmS4fpKIjG5W78GWEVOu.jpeg']     
+    ['Back','2ny9VRoyfo8cI4PNhImDQJjjBvgdxvl13FAGlwry.jpeg']
+    ['Front','bmS1r8WQ7jJX6Dxg3PeipEz4J9ju2O31flk5ioeu.jpeg']    
+    
+    'Model  Mazda, Bongo'
+    ['Sideways Left','gSjDM7xZRWNv17QLTNtFkC0pitrEes94bnOTBcYM.jpeg']    
+    ['Sideways Right ', 'n1ZIE2OSgJdLL1WNy8ehu8ZrcfqwTP5GtFqOYlTj.jpeg']     
+    ['Back','ZJfo6SPtFy583fasLO2uKu1T80ObWRLFOctr6dGg.jpeg']
+    ['Front','jv9Rm3wN9uAMabKiIUmVttDujyAXhpqWDNnpocWT.jpeg']    
+    ['Inside Driver','aEMonOVjjpEp6mIjINPkpaTQszAG5xT1SU0pJCUY.jpeg']    
+
+    'Model   Isuzu, FRR'
+    ['Engine','aRJM9bG14DndCJft5PJD7geo8UHYKfuwQO3TTM8a.png']    
+    ['Sideways Back Left ', 'yuRBXPo8f4oKq4kVjr6Mp38pr8oGJbPcA8U4rNaa.png']     
+    ['Front Left','N6v1RLoOVBZMironTkhfuKWDTkNih5aDL7LrVbol.png']
+    ['Back Right','2czUndQQpHXDdaL9Ew966ufFdMcXRg4wWVvkw9m4.png']    
+
+    'Model  Toyota, Ipsum'
+    ['Back','EucNvP7uM4GdnfyhRt3RmYSopWafQHFPvJS3hM6i.jpeg']    
+    ['Sideways Back Right ', '1A8YnJK4KdVE7mfZFajBQGlRGm8OCUy7daO8FO2k.jpeg']     
+    ['SidewaysBack Left','2BqC8TtyYStnrD7DQmbbpjNHiFmeooSZrZJaevyY.jpeg']
+    ['Front Right','V03uKljFZGZ4HogrMkXPUcidhqSTx29GZeXihjOc.jpeg']    
+    ['Front Left','UF7DRjnhnJYCOyI5YAsTlc4HM0u7UEzfkYLZc0qP.jpeg']    
+
+    'Model  Toyota, Hiace'
+    ['Front','Zqw8UJijsDNl2YRXw5bIWFLwaGbKyufYSCl15F15.jpeg']    
+    ['Back', '1H01tuavqHTrM7tb5matlo7sxfwPgNqh4vI4F91S.jpeg']     
+    
+    'Model   Isuzu, FRR'
+    ['Front Right','yoaKQSRgSoTHoc1EYCiKZ0fNlhJpa7rRHQuDYMGW.jpeg']    
+    ['Front', 'QLEGlHVoInZ80VeErxeKj7loNGrVDIzLk6oi6mVw.jpeg']     
+    ['Back','zeafhlYYBxJwXciISy7nYxXNaCDpZrUpsy5pRBXX.jpeg']
+    ['Back Right','YB6JzCgs8lnbUprBp6Ax33idbKX07igwWlN7GLIY.jpeg']    
+
+    'Model  Volkswagen, TIGUAN'
+    ['Sideways Left','Hf0dsbH20VtZE2CuOt88G5NbMFhGcKInKmmaHiBx.jpeg']    
+    ['Back', 'z81g0ebnL544qiQGvgaYjc6FYYgUzGcirBIrnWOZ.jpeg']     
+    ['Sideways Left','AjZ5i8jLM1EU2g1FYShCGKpzIhZmpEdkRdealuiK.jpeg']
+   
+"
+?>
